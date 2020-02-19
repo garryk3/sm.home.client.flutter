17d636350360a6e9609cd20af48567a073948995
garryk3 <egoruum@gmail.com>
2020-02-19 18:51:58 +0300

    leatn

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import '../components/header.dart';
 
-class Home extends StatefulWidget {}
+class Home extends StatefulWidget {
+  createState() {
+    return HomeState();
+  }
+}
 
 class HomeState extends State<Home> {
+  int counter = 0;
 
     @override
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: Header(),
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
-          onPressed: () { print('test press'); },
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          },
           child: Icon(Icons.add)
         ),
       );
